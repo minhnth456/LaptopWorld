8399c2c9b614e615c0c3612c760f1aa38a9b9973
minhnth456 <minhnth456@gmail.com>
2023-11-07 11:57:42 +0700

    lan2

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ function tudong() {
         setTimeout(function () {
             document.getElementById("anh-tu-dong").src = `img/anh${soAnh}.jpg`;
             soAnh++;
-            if (soAnh > 5) {
+            if (soAnh > 3) {
                 soAnh = 0;
             }
-        }, 500); /* Đợi một chút trước khi thay đổi hình ảnh */
+        }, 200); /* Đợi một chút trước khi thay đổi hình ảnh */
         setTimeout(function () {
             document.getElementById("anh-tu-dong").style.transform = "scale(1)"; /* Trả về tỷ lệ ban đầu */
-        }, 1000);
+        }, 200);
     }, 2000);
 }
 tudong();
